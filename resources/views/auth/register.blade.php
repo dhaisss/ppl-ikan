a01b9a891f8cc390e9bb5e5b19f9c30ed2a41339
dhaisss <dhaisfirmansyah16@gmail.com>
2018-09-12 20:13:45 +0700

    First Commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,129 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('alamat') ? ' has-error' : '' }}">
+                            <label for="alamat" class="col-md-4 control-label">Alamat</label>
+
+                            <div class="col-md-6">
+                                <input id="alamat" type="text" class="form-control" name="alamat" value="{{ old('alamat') }}" required autofocus>
+
+                                @if ($errors->has('alamat'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('alamat') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('kecamatan') ? ' has-error' : '' }}">
+                          <label class="col-md-4 control-label">Kecamatan</label>
+
+                          <div class="col-md-6">
+                               <input type="text" class="form-control" name="kecamatan" value="{{ old('kecamatan') }}">
+
+                          </div>
+                      </div>
+
+                      <div class="form-group{{ $errors->has('kabupaten') ? ' has-error' : '' }}">
+                          <label class="col-md-4 control-label">Kabupaten</label>
+
+                          <div class="col-md-6">
+                               <input type="text" class="form-control" name="kabupaten" value="{{ old('kabupaten') }}">
+
+                          </div>
+                      </div>
+
+                      <div class="form-group{{ $errors->has('provinsi') ? ' has-error' : '' }}">
+                          <label class="col-md-4 control-label">Provinsi</label>
+
+                          <div class="col-md-6">
+                               <input type="text" class="form-control" name="provinsi" value="{{ old('provinsi') }}">
+
+                          </div>
+                      </div>
+
+                        <div class="form-group{{$errors->has ('level') ? 'has-error':''}}">
+                            <label class="col-md-4 control-label">Sebagai</label>
+
+                        <div class="col-md-6">
+                            <select name="level" class="form-control">
+                                <option value="1">Agen</option>
+                                <option value="2">Pengusaha</option>
+                            </select>
+                        </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/sidebarAgen.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Golek Iwak</title>
+	<link rel="stylesheet" href="{{url('assets/plugins/bootstrap/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{url('assets/plugins/fontawesome/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{url('assets/css/theme-floyd.css')}}">
+	<link rel="stylesheet" href="{{url('assets/css/theme-helper.css')}}">
+	<style>
+            html, body {
+                background: url("bg/WALLPAPER 1.jpg");
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+            </style>
+</head>
+
+<body>
+	<div id="wrapper">
+		<div id="sidebar">
+			<div id="sidebar-wrapper">
+				<div class="sidebar-title">
+
+				</div>
+				<div class="sidebar-avatar">
+
+					<div class="sidebar-avatar-text">  {{ Auth::user()->name }}  </div>
+				</div>
+				<ul class="sidebar-nav">
+
+					<!-- <li><a href="{{ url('/profilAgen') }}"> <span>Ubah Profil</span></a></li>
+					 -->
+					<li><a href="{{ url('/profilAgen/'.Auth::user()->id) }}"> <span>Profil</span></a></li>
+
+					<li><a href="{{ url('/daftarPengusaha') }}"> <span>Daftar Pengusaha</span></a></li>
+
+					<li><a href="{{ url('/buatPenawaran') }}"> <span>Buat Penawaran Ikan</span></a></li>
+
+					<li><a href="{{ url('/daftarPenawaran/'.Auth::user()->id) }}"><span>Daftar Penawaran </span></a></li>
+
+					<li><a href="{{ url('/notifikasiAgen/'.Auth::user()->id) }}"><span>Notifikasi</span></a></li>
+
+					<li><a href="{{ url('/transaksiAgen/'.Auth::user()->id) }}"><span>Transaksi</span></a></li>
+
+					<li><a href="{{ route('logout') }}"
+							onclick="event.preventDefault();
+											 document.getElementById('logout-form').submit();">
+						<span>Logout</span>
+					</a>
+
+					<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+							{{ csrf_field() }}
+					</form>
+				</li>
+
+<!--
+							<li>
+									<a href="{{ route('logout') }}"
+											onclick="event.preventDefault();
+															 document.getElementById('logout-form').submit();">
+											Logout
+									</a>
+
+									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+											{{ csrf_field() }}
+									</form>
+							</li> -->
+
+
+					</ul>
+				<div class="sidebar-footer">
+					<hr style="border-color: #DDD">
+					</a><br>
+				</div>
+				<div class="sidebar-avatar-image"><img src="assets/images/logo2.png" alt=""></div><br>
+			</div>
+		</div>
+
+			@yield('content')
+	</div>
+</body>
+<script src="{{url('assets/plugins/jquery/jquery-3.1.1.min.js')}}"></script>
+<script src="{{url('assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{url('assets/js/theme-floyd.js')}}"></script>
+</html>
